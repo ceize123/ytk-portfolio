a859f37eff8237ba7c4e00ae06c91a18f47d2cb8
ytkuo <hahaeating1@gmail.com>
2023-02-09 23:17:38 -0500

    change id

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,16 +8,17 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
+			<Script async strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS}`}></Script>
 			<Script strategy='lazyOnload' id='script'>
-					{`
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-					`}
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', ${process.env.NEXT_PUBLIC_ANALYTICS});
+				`}
 			</Script>
+
 			<Head>
 					<title>Yi-Tso Kuo</title>
 					<meta name="viewport" content="initial-scale=1, width=device-width" />
