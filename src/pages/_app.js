c24095d66f547d7b12144f2b906f7aee8d8ce170
ytkuo <hahaeating1@gmail.com>
2023-02-09 22:44:59 -0500

    add google analystics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
 							window.dataLayer = window.dataLayer || [];
 							function gtag(){dataLayer.push(arguments);}
 							gtag('js', new Date());
-							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+							gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
 					`}
 			</Script>
 			<Head>
