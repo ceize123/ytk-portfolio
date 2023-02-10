01fdb8723376a56b8f0a6f27d045bcf4422b53b7
ytkuo <hahaeating1@gmail.com>
2023-02-09 23:23:39 -0500

    test google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,14 +8,16 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<Script async strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS}`}></Script>
+			<Script async strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
 			<Script strategy='lazyOnload' id='script'>
 				{`
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
 
-					gtag('config', ${process.env.NEXT_PUBLIC_ANALYTICS});
+					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+						page_path: window.location.pathname,
+					});
 				`}
 			</Script>
 
