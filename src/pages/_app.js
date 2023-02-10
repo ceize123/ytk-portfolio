7d80f02294c964b954c40a6906350e9f5df7a54b
ytkuo <hahaeating1@gmail.com>
2023-02-09 23:27:11 -0500

    remove script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,27 +3,13 @@ import Navbar from '../components/Navbar'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<Script async strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
-			<Script strategy='lazyOnload' id='script'>
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-
-					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-						page_path: window.location.pathname,
-					});
-				`}
-			</Script>
-
 			<Head>
 					<title>Yi-Tso Kuo</title>
-					<meta name="viewport" content="initial-scale=1, width=device-width" />
+					<meta name='viewport' content='initial-scale=1, width=device-width' />
 			</Head>
 			<Navbar />
 			<Component {...pageProps} />
