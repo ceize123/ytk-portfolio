ca2bd902d0425b63432a8f22787ecf2c4369e7db
ytkuo <hahaeating1@gmail.com>
2023-02-10 01:00:01 -0500

    add link to github, linkedIn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
-import { FiLinkedin, FiGithub } from 'react-icons/fi';
+import { FiLinkedin, FiGithub } from 'react-icons/fi'
+import Link from 'next/link'
 
 export default function Footer() {
 	return (
 		<footer className='pt-24 pb-8'>
 			<div className='md:fixed md:bottom-0 md:left-6 flex justify-center md:block mb-2 md:mb-0'>
 				<div className='icon-block cursor-pointer md:mb-3 p-2'>
-					<FiGithub className='icon text-yellow text-2xl' />
+					<Link href='https://github.com/ceize123' target='_blank'>
+						<FiGithub className='icon text-yellow text-2xl' />
+					</Link>
 				</div>
 				<div className='icon-block cursor-pointer md:mb-3 p-2 mx-2 md:mx-0'>
-					<FiLinkedin
-						className='icon text-yellow text-2xl' />
+					<Link href='https://www.linkedin.com/in/yi-tso-kuo/' target='_blank'>
+						<FiLinkedin
+							className='icon text-yellow text-2xl' />
+					</Link>
 				</div>
 				<div className='h-20 w-px bg-secondary mx-auto hidden md:block'></div>
 			</div>
