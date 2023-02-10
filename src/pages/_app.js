bcab27197699b98dcddb1a815d7051d63edaca53
ytkuo <hahaeating1@gmail.com>
2023-02-09 22:25:31 -0500

    add google analystics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,26 @@ import '../styles/style.css'
 import Navbar from '../components/Navbar'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
+			<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+			<Script strategy='lazyOnload' id='script'>
+					{`
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+					`}
+			</Script>
+			<Head>
+					<title>Yi-Tso Kuo</title>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
 			<Navbar />
 			<Component {...pageProps} />
 			<Contact />
